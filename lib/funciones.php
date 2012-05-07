c70f9a5f5d24008e52637f5d138459c4f6992fe7
Agus Black Pc <jask.zemula@gmail.com>
2012-05-07 19:06:10 +0200

    login.php e index.php para comprobar usuarios.

---
+++ lib/funciones.php
@@ -0,0 +1,48 @@
+<?php
+
+class User{
+	
+	public function getInfoUser($nick, $email)
+	{
+
+	}
+
+	public function changeInfoUser()
+	{
+
+	}
+
+	public function checkUser($nick, $pass, $email = '')
+	{
+		$this->user = $nick;
+		$this->pass = $pass;
+		$this->email = $email;
+	}
+
+	public function newUser()
+	{
+
+	}
+
+	public function editUser()
+	{
+
+	}
+
+	public function promoteUser()
+	{
+
+	}
+
+	public function deleteUser()
+	{
+
+	}
+
+	public function banUser()
+	{
+
+	}
+}
+
+?>
